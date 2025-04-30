577f38fb444427e646953f1bbc0aa38f816bf1e8
MJ1717 <bananahamo77@gmail.com>
2025-04-30 13:42:06 -0400

    added /task

--- server/server.js
+++ server/server.js
@@ -6,16 +6,42 @@ const app = express();
 
 const PORT = 3000;
 
+const tasks = [];
+
+
 //when front end gives information it turns into js so we can use
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('it is working');
+    res.send('he334o');
 });
 
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
+});
+
+
+
+//trigger when frontend send a post request on /tasks
+app.post('/tasks', (req, res) => {
+    const task = req.body;
+  
+    if (!task.title) {
+        //400 for bad request
+      return res.status(400).json({ error: "u need title" });
+    }
+  
+    tasks.push(task);
+  
+    //201 for created succesfuly
+    res.status(201).json({ message: "task added successfully", task });
+  });
+
+
+
+app.get('/tasks', (req, res) => {
+    res.json(tasks);  // return all stored tasks
 });
 
   
