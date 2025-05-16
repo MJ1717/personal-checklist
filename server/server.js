a5c09a7d7e8d2949ee057471b22a1bf67c241e6b
MJ1717 <bananahamo77@gmail.com>
2025-05-16 15:03:02 -0400

    added add.delete in server.js, and modify loadtask() so we can delete tasks

--- server/server.js
+++ server/server.js
@@ -52,4 +52,20 @@ app.get('/tasks', (req, res) => {
     res.json(tasks);  // return all stored tasks
 });
 
+
+//app.delete is triggered when frontend want to delete some information in the server
+// thing after : is reqeust parameter. so now, index is the req parameter
+app.delete('/tasks/:index', (req, res) => {
+  //parseint is convert string to num. req.params.blah is getting the req param's value
+  const index = parseInt(req.params.index);
+
+  if (index >= 0 && index < tasks.length) {
+    tasks.splice(index, 1); //splice is javasciprt builtin. a.splice(start index, num of items want to remove)
+    res.json({message: "task deleted"});
+  }
+  else {
+    res.status(404).json({error: "task not there"});
+  }
+});
+
   
